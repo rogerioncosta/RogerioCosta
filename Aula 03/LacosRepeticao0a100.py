488b8afc0bdfce10af28b1b7aee65259f9e5ff80
Rogerio Costa <rogerioncosta1@gmail.com>
2024-09-28 12:41:11 -0300

    primeiro commit

---
+++ Aula 03/LacosRepeticao0a100.py
@@ -0,0 +1,7 @@
+# Crie um programa que escreva os números de 0 a 100
+
+numero = 1
+
+while numero <= 100:
+    print(f"Número: {numero}")
+    numero = numero + 1
---
+++ Aula 03/lacosRepeticaoExercicios.py
@@ -0,0 +1,17 @@
+# Atividades Listas e Laços de Repetição
+
+# 1- Crie um programa que simule um sistema básico de reserva de voos. O programa deve permitir que o usuário adicione reservas de voos enquanto desejar. Cada reserva deve incluir o nome do passageiro e o destino. Ao final, o programa deve listar todas as reservas feitas.
+reservas = []
+
+
+
+# 2  - Crie um programa que solicite números para o usuário até que ele digite 0. Ao final deve ser perguntado ao usuário se é ordem crescente e decrescente e mostrar a lista ordenada.
+
+# 3 - Crie um programa que solicite ao usuário 5 números e ao final imprima a soma de todos eles.
+
+# 4 - Crie um programa que solicite ao usuário o nome e o salário de seus funcionários. Ao final imprima o nome e o salário de todos os funcionários, bem como a média salarial da empresa, o maior salário e o menor salário e o total gasto com salários.
+
+# 5 - Uma escola precisa de ajuda para organizar as notas dos alunos. Crie um programa que leia o nome de um aluno e suas 4 notas. O programa deve continuar lendo os nomes e notas até que o usuário não deseje mais adicionar alunos. Em seguida, o programa deve exibir o nome e a média de cada um dos alunos.
+# Extra:
+# - Permita que o usuário possa escolher quantas notas cada aluno terá.
+# - Mostre uma média geral da turma.
