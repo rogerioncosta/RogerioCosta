453a2a1f70c299cb30b2c9df0b4c1d88db0f74eb
Rogerio <rogerioncosta1@gmail.com>
2024-10-01 16:34:34 -0300

    Exercício 4 salário

---
+++ Aula 03/5_notas.py
@@ -0,0 +1,5 @@
+# 5 - Uma escola precisa de ajuda para organizar as notas dos alunos. Crie um programa que leia o nome de um aluno e suas 4 notas. O programa deve continuar lendo os nomes e notas até que o usuário não deseje mais adicionar alunos. Em seguida, o programa deve exibir o nome e a média de cada um dos alunos.
+# Extra:
+# - Permita que o usuário possa escolher quantas notas cada aluno terá.
+# - Mostre uma média geral da turma.
+
