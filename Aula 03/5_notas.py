24fef565b38aeec23cc44bb64ecde0ba8a268586
Rogerio <rogerioncosta1@gmail.com>
2024-10-02 17:15:39 -0300

    Inicio da lógica do exercício 5_notas.py

--- Aula 03/5_notas.py
+++ Aula 03/5_notas.py
@@ -3,3 +3,25 @@
 # - Permita que o usuário possa escolher quantas notas cada aluno terá.
 # - Mostre uma média geral da turma.
 
+listaAlunos = []
+listaNotas = []
+
+continuar = "s"
+
+while continuar == "s":
+    qtde_notas_aluno = 0
+    nome_aluno = input("Digite o nome do aluno: ")
+    listaAlunos.append(nome_aluno)
+
+    qtde_nota_aluno = int(input("Quantas notas esse aluno terá? "))   
+
+    while qtde_notas_aluno < qtde_nota_aluno:
+        nota = float(input("Digite a nota: "))
+        listaNotas.append(nota)
+        qtde_notas_aluno += 1
+        
+    continuar = input("Deseja continuar? (s/n): \n")
+
+    # for i in range(len(listaAlunos)):
+    #     media = listaNotas[i] / len(listaNotas) 
+    #     print(f"O aluno {listaAlunos[i]} obteve a média: {media}:.2f")
