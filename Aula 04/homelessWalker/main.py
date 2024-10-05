3ac0f64bf0e620814e58cbdf434c3c535aca399d
rogerioncosta <rogerioncosta1@gmail.com>
2024-10-05 17:50:50 -0300

    Animação jogo

--- Aula 04/homelessWalker/main.py
+++ Aula 04/homelessWalker/main.py
@@ -15,16 +15,33 @@ folhaSpritesWalk = pygame.image.load("assets/Homeless_1/Walk.png").convert_alpha
 
 # Define os frames
 framesIdle = []
+framesWalk = []
 
 for i in range(6):
     frame = folhaSpritesIdle.subsurface(i * 128, 0, 128, 128) # A imagem tem 768 largura, e tem 6 imagens que quero dividir
     frame = pygame.transform.scale(frame, (256, 256))
     framesIdle.append(frame)
 
+# **
+for i in range(8):
+    frame = folhaSpritesWalk.subsurface(i * 128, 0, 128, 128) # A imagem tem 768 largura, e tem 6 imagens que quero dividir
+    frame = pygame.transform.scale(frame, (256, 256))
+    framesWalk.append(frame)
+
 # Variáveis da animação do personagem parado
 indexFrameIdle = 0
 tempoAnimacaoIdle = 0.0
 velocidadeAnimacaoIdle = 10
+# **
+indexFrameWalk = 0
+tempoAnimacaoWalk = 0.0
+velocidadeAnimacaoWalk = 10
+
+# Retangulo do personagem
+personagemRect = framesIdle[0].get_rect(midbottom=(100, 480))
+personagemWalk = framesWalk[0].get_rect(midbottom=(100, 480))
+
+gravidade = 1
 
 while True:
     for event in pygame.event.get():
@@ -36,13 +53,42 @@ while True:
 
     # Atualiza a animação do personagem parado
     tempoAnimacaoIdle += dt
+    tempoAnimacaoWalk += dt
 
     if tempoAnimacaoIdle >= 1 / velocidadeAnimacaoIdle:
         indexFrameIdle = (indexFrameIdle + 1) % len(framesIdle)
         tempoAnimacaoIdle = 0.0
 
+    if tempoAnimacaoWalk >= 1 / velocidadeAnimacaoWalk:
+        indexFrameWalk = (indexFrameWalk + 1) % len(framesWalk)
+        tempoAnimacaoWalk = 0.0
+
+    # Movimenta o personagem no eixo X
+    teclas = pygame.key.get_pressed()
+
+
+    if teclas[pygame.K_LEFT]:
+        personagemWalk.x -= 200 * dt # Movimenta pra esquerda
+    if teclas[pygame.K_RIGHT]:
+        personagemWalk.x += 200 * dt # Movimenta pra direita
+    if teclas[pygame.K_SPACE]:
+        if personagemWalk.centery == 330:
+            gravidade = -30
+
+    # Gravidade aumenta
+    gravidade += 3
+
+    personagemRect.y += gravidade
+    personagemWalk.y += gravidade
+
+    if personagemWalk.centery >= 330:
+        personagemWalk.centery = 330
+
     # Desenha o personagem
-    tela.blit(framesIdle[indexFrameIdle], (600, 250))
+    tela.blit(framesIdle[indexFrameIdle], personagemRect)
+
+    # Desenha um retangulo em volta do personagem
+    pygame.draw.rect(tela, (0, 0, 0), personagemWalk, 2)
 
     pygame.display.update()
     dt = relogio.tick(60) / 1000
