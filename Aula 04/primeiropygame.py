39adaf4f1017e2e9f9e0659914992abb0e854149
rogerioncosta <rogerioncosta1@gmail.com>
2024-10-05 14:19:03 -0300

    Gerenciamento de elementos

--- Aula 04/primeiropygame.py
+++ Aula 04/primeiropygame.py
@@ -12,6 +12,12 @@ pygame.display.set_caption("Hello Games!")
 # Define um relógio para controlar o FPS
 relogio = pygame.time.Clock()
 
+posicaoBola = pygame.Vector2(450, 250)
+dt = 0
+direcaoY = 1
+direcaoX = 1
+velBola = 100
+
 while True:
     # Lida com os eventos da aplicação
     for evento in pygame.event.get():
@@ -19,12 +25,29 @@ while True:
         if evento.type == pygame.QUIT:
             pygame.quit() # Fechando o pygame
 
-        # Preenche a tela com uma cor
-        tela.fill((40, 200, 100))
+    # Preenche a tela com uma cor
+    tela.fill((40, 200, 100))
 
-        
+    # Desenha um círculo na tela
+    pygame.draw.circle(tela, (10, 255, 200), posicaoBola, 50)
+    
+    posicaoBola.y += velBola * direcaoY * dt
+    posicaoBola.x += velBola * direcaoX * dt
 
-        # Atualiza a tela
-        pygame.display.update()
+    # if posicaoBola.y >= 450:
+    #     direcaoY = -1
+    # elif posicaoBola.y <= 50:
+    #     direcaoY = 1
+    if posicaoBola.y >= 450 or posicaoBola.y <= 50:
+        direcaoY *= -1 # Inverte a direção
+        velBola += 10
 
-        relogio.tick(60) # Define a quantidade de FPS
+    if posicaoBola.x >= 850 or posicaoBola.x <= 50:
+        direcaoX *= -1 # Inverte a direção
+        velBola += 10
+
+    # Atualiza a tela
+    pygame.display.update()
+    dt = relogio.tick(60) / 1000 # Define a quantidade de FPS
+
+    # relogio.tick(60) # Define a quantidade de FPS
